4f6eede0fe009816ca9bcacc205603b082fbdcac
javvadigihub <33460011+javvadigithub@users.noreply.github.com>
2023-04-28 11:14:42 +0530

    Final Push
    Updated final push

--- src/inventory/views.py
+++ src/inventory/views.py
@@ -5,11 +5,26 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator
 import json
-from django.http import JsonResponse
-# from userpreferences.models import UserPreference
+from django.http import JsonResponse, HttpResponse
+from userconfig.models import UserConfig
 import datetime
+import csv
 
 # Create your views here.
+
+
+    
+def search_expenses(request):
+    if request.method =='POST':
+        search_str = json.loads(request.body).get('searchText')
+        expenses = Expense.objects.filter(
+            amount__istartswith=search_str, owner=request.user) | Expense.objects.filter(
+            date__istartswith=search_str, owner=request.user) | Expense.objects.filter(
+            description__icontains=search_str, owner=request.user) | Expense.objects.filter(
+            category__icontains=search_str, owner=request.user)
+        data = expenses.values()
+        return JsonResponse(list(data), safe=False)
+    
 
 @login_required(login_url='/auth/login')
 def index(request):
@@ -18,9 +33,11 @@ def index(request):
     paginator = Paginator(expenses, 5)
     page_number = request.GET.get('page')
     page_obj=Paginator.get_page(paginator, page_number)
+    currency = UserConfig.objects.get(user=request.user).currency
     context={
         'expenses': expenses,
-        'page_obj': page_obj
+        'page_obj': page_obj,
+        'currency': currency
 
     }
     return render (request, 'inventory/index.html', context)
@@ -96,3 +113,48 @@ def delete_expense(request, id):
     expense.delete()
     messages.error(request, 'Requested Removed successfully')
     return redirect('inventory')
+
+def expense_category_summary(request):
+    todays_date = datetime.date.today()
+    six_months_ago = todays_date-datetime.timedelta(days=30*6)
+    expenses = Expense.objects.filter(owner=request.user,
+                                      date__gte=six_months_ago, date__lte=todays_date)
+    finalrep = {}
+
+    def get_category(expenses):
+        return expenses.category
+    category_list = list(set(map(get_category, expenses)))
+
+    def get_expense_category_amount(category):
+        amount = 0
+        filtered_by_category = expenses.filter(category=category)
+
+        for item in filtered_by_category:
+            amount += item.amount
+        return amount
+
+    for x in expenses:
+        for y in category_list:
+            finalrep[y] = get_expense_category_amount(y)
+
+    return JsonResponse({'expense_category_data': finalrep}, safe=False)
+
+def stats_view(request):
+    return render(request, 'inventory/stats.html')
+
+
+def export_csv(request):
+
+    response = HttpResponse(content_type='text/csv ')
+    response['Content-Disposition'] = 'attachment; filename=Expenses'+ \
+       str( datetime.datetime.now())+'.csv'
+    writer=csv.writer(response)
+    writer.writerow(['Amount', 'Descrption', 'Category', 'Date'])
+
+    expenses = Expense.objects.filter(owner=request.user)
+
+    for expense in expenses:
+        writer.writerow([expense.amount, expense.description,
+                         expense.category, expense.date])
+        
+    return response
